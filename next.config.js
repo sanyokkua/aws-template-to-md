cac5c5b93a51808f87eb653da02f5d821a29a063
Oleksandr Kostenko <Oleksandr.kostenko@outlook.com>
2023-10-09 15:01:05 +0300

    Added configs for exporting static site

---
+++ next.config.js
@@ -0,0 +1,23 @@
+/** @type {import("next").NextConfig} */
+
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+
+let assetPrefix = "";
+let basePath = "";
+
+if (isGithubActions) {
+    // trim off `<owner>/`
+    const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, "");
+
+    assetPrefix = `/${repo}/`;
+    basePath = `/${repo}`;
+}
+
+const nextConfig = {
+    reactStrictMode: true,
+    output: "export",
+    assetPrefix: assetPrefix,
+    basePath: basePath,
+};
+
+module.exports = nextConfig;
